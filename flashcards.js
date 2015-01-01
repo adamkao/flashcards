2a724af71bb4f9d3cb53e8c809cdf18dfa8efbdf
Adam Kao <adamkao@gmail.com>
2015-01-01 15:50:07 -0800

    simple flashcard program

---
+++ flashcards.js
@@ -0,0 +1,176 @@
+var flashcards = [
+	[ "los derechos humanos", "human rights", 0, 0 ],
+	[ "la desobediencia civil", "civil disobedience", 0, 0 ],
+	[ "la desigualdad", "inequality", 0, 0 ],
+	[ "el juez", "judge", 0, 0 ],
+	[ "la injusticia", "injustice", 0, 0 ],
+	[ "la libertad", "freedom", 0, 0 ],
+	[ "la lucha", "struggle", 0, 0 ],
+	[ "el tribunal", "court", 0, 0 ],
+	[ "abusar", "to abuse", 0, 0 ],
+	[ "aprobar una ley", "to pass a law", 0, 0 ],
+	[ "convocar", "to summon", 0, 0 ],
+	[ "defender", "to defend", 0, 0 ],
+	[ "derogar", "to abolish", 0, 0 ],
+	[ "encarcelar", "to imprison", 0, 0 ],
+	[ "juzgar", "to judge", 0, 0 ],
+	[ "analfabeto", "illiterate", 0, 0 ],
+	[ "desigual", "unequal", 0, 0 ],
+	[ "injusto", "unfair", 0, 0 ],
+	[ "oprimido", "oppressed", 0, 0 ],
+	[ "el abuso", "abuse", 0, 0 ],
+	[ "la armada", "navy", 0, 0 ],
+	[ "la bandera", "flag", 0, 0 ],
+	[ "la creencia", "belief", 0, 0 ],
+	[ "la crueldad", "cruelty", 0, 0 ],
+	[ "la democracia", "democracy", 0, 0 ],
+	[ "la dictadura", "dictatorship", 0, 0 ],
+	[ "el ejército", "army", 0, 0 ],
+	[ "el gobierno", "government", 0, 0 ],
+	[ "la guerra civil", "civil war", 0, 0 ],
+	[ "el partido político", "political party", 0, 0 ],
+	[ "la paz", "peace", 0, 0 ],
+	[ "el poder", "power", 0, 0 ],
+	[ "la política", "politics", 0, 0 ],
+	[ "las relaciones exteriores", "foreign relations", 0, 0 ],
+	[ "la victoria", "victory", 0, 0 ],
+	[ "dedicarse a", "to devote oneself to", 0, 0 ],
+	[ "elegir", "to elect", 0, 0 ],
+	[ "ganar las elecciones", "to win elections", 0, 0 ],
+	[ "gobernar", "to govern", 0, 0 ],
+	[ "influir", "to influence", 0, 0 ],
+	[ "votar", "to vote", 0, 0 ],
+	[ "conservador", "conservative", 0, 0 ],
+	[ "liberal", "liberal", 0, 0 ],
+	[ "pacífico", "peaceful", 0, 0 ],
+	[ "pacifista", "pacifist", 0, 0 ],
+	[ "el abogado", "lawyer", 0, 0 ],
+	[ "el activista", "activist", 0, 0 ],
+	[ "el ladrón", "theif", 0, 0 ],
+	[ "el manifestante", "demonstrator", 0, 0 ],
+	[ "el político", "politician", 0, 0 ],
+	[ "el presidente", "president", 0, 0 ],
+	[ "el terrorista", "terrorist", 0, 0 ],
+	[ "la víctima", "victim", 0, 0 ],
+	[ "la amenaza", "threat", 0, 0 ],
+	[ "el arma", "weapon", 0, 0 ],
+	[ "el escándalo", "scandal", 0, 0 ],
+	[ "la inseguridad", "insecurity", 0, 0 ],
+	[ "el temor", "fear", 0, 0 ],
+	[ "el terrorismo", "terrorism", 0, 0 ],
+	[ "la violencia", "violence", 0, 0 ],
+	[ "chantajear", "to blackmail", 0, 0 ],
+	[ "destrozar", "to destroy", 0, 0 ],
+	[ "espiar", "to spy", 0, 0 ],
+	[ "huir", "to flee", 0, 0 ],
+	[ "pelear", "to fight", 0, 0 ],
+	[ "secuestrar", "to kidnap", 0, 0 ],
+	[ "el alumno", "student", 0, 0 ],
+	[ "la autoridad", "authority", 0, 0 ],
+	[ "el chaval", "kid", 0, 0 ],
+	[ "la confianza", "trust", 0, 0 ],
+	[ "la diversidad", "diversity", 0, 0 ],
+	[ "la doble moral", "double standard", 0, 0 ],
+	[ "la hipocresía", "hypocrisy", 0, 0 ],
+	[ "el instituto", "high school", 0, 0 ],
+	[ "el pañuelo", "headscarf", 0, 0 ],
+	[ "el rato", "a while", 0, 0 ],
+	[ "la regla", "rule", 0, 0 ],
+	[ "ceder", "to give up", 0, 0 ],
+	[ "confiar", "to trust", 0, 0 ],
+	[ "disentir", "to dissent", 0, 0 ],
+	[ "pegar", "to hit", 0, 0 ],
+	[ "laico", "secular", 0, 0 ],
+	[ "musulmán", "Muslim", 0, 0 ],
+	[ "raro", "weird", 0, 0 ],
+	[ "la ejecución", "execution", 0, 0 ],
+	[ "el fracaso", "failure", 0, 0 ],
+	[ "la fuerza", "force", 0, 0 ],
+	[ "el golpe de estado", "coup d'état", 0, 0 ],
+	[ "la huelga", "strike", 0, 0 ],
+	[ "el informe", "report", 0, 0 ],
+	[ "el juicio", "judgement", 0, 0 ],
+	[ "la ley", "law", 0, 0 ],
+	[ "el orgullo", "pride", 0, 0 ],
+	[ "el secuestro", "kidnapping", 0, 0 ],
+	[ "la trampa", "trap", 0, 0 ],
+	[ "derrocar", "to overthrow", 0, 0 ],
+	[ "derrotar", "to defeat", 0, 0 ],
+	[ "ejercer el poder", "to exercise power", 0, 0 ],
+	[ "encabezar", "to lead", 0, 0 ],
+	[ "fortalecer", "to strengthen", 0, 0 ],
+	[ "promulgar", "to enact (a law)", 0, 0 ],
+	[ "tener derecho a", "to have the right to", 0, 0 ],
+	[ "rescatado", "rescued", 0, 0 ],
+	[ "el alma", "soul", 0, 0 ],
+	[ "la astilla", "splinter", 0, 0 ],
+	[ "la cárcel", "prison", 0, 0 ],
+	[ "el castigo", "punishment", 0, 0 ],
+	[ "la celda", "cell", 0, 0 ],
+	[ "la censura", "censorship", 0, 0 ],
+	[ "el orgullo", "pride", 0, 0 ],
+	[ "el preso", "prisoner", 0, 0 ],
+	[ "la rebeldía", "rebelliousness", 0, 0 ],
+	[ "la reja", "iron bar", 0, 0 ],
+	[ "la sangre", "blood", 0, 0 ],
+	[ "la tinta", "ink", 0, 0 ],
+	[ "ahogar", "to stifle", 0, 0 ],
+	[ "callar", "to silence", 0, 0 ],
+	[ "hundir", "to sink", 0, 0 ],
+	[ "resistir", "to resist", 0, 0 ],
+	[ "vigilar", "to keep watch on", 0, 0 ],
+];
+
+function shuffle( array ) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor( Math.random() * currentIndex );
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
+
+$( document ).ready( function() {
+	var answer = "", correct = "", last = 0, attempts = 0, success = 0, pass = 1, rand = 0, prob = 0;
+	
+	shuffle( flashcards );
+	$( "#question" ).val( pass + ":  " + flashcards[last][1] );	
+	attempts = flashcards[last][3];
+	success = flashcards[last][2];
+
+	$( "#done" ).click( function( e ) {
+		attempts++;
+		answer = $( "#answer" ).val();
+		if (flashcards[last][0] == answer) {
+			correct = "Right!";
+			success++;
+		} else {
+			correct = "Wrong!";
+		}
+		correct += " " + success + "/" + attempts;
+		$( "#response" ).val( correct + " " + flashcards[last][1] + ":\n" + flashcards[last][0] + "\n" + prob );
+
+		flashcards[last][3] = attempts;
+		flashcards[last][2] = success;
+
+		$( "#answer" ).val( "" );
+		$( "#answer" ).focus();
+
+		do {
+			last++;
+			if (last >= flashcards.length) {
+				shuffle( flashcards );
+				pass++;
+				last = 0;
+			}
+			attempts = flashcards[last][3];
+			success = flashcards[last][2];
+			prob = (success*success)/((attempts*attempts) + 1);
+		    rand = Math.random();
+		} while (rand < prob);
+		$( "#question" ).val( pass + ":  " + flashcards[last][1] );	
+	} );
+} );
